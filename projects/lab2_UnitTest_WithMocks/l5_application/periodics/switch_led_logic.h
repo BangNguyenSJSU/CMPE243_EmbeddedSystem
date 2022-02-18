7356f5f9dbef4ed4824add1336efbad4983c0c1c
Bang(Max) Nguyen <38081550+BangNguyenSJSU@users.noreply.github.com>
2022-02-17 22:38:39 -0800

    Init Lab2 Folder
    Lab2_path: projects/lab2_UnitTest_WithMocks

---
+++ projects/lab2_UnitTest_WithMocks/l5_application/periodics/switch_led_logic.h
@@ -0,0 +1,13 @@
+//
+
+#pragma once
+
+#include "board_io.h"
+#include "gpio.h"
+#include <stdint.h>
+
+/* Initialize function for switch and LED */
+void switch_led_logic_initialize();
+
+/*  Switch LED Run Once */
+void switch_led_logic_run_once();
