0beeb8531eaf7e3a1f8c44aad0996b44915a9225
Bang(Max) Nguyen <38081550+BangNguyenSJSU@users.noreply.github.com>
2022-02-19 17:42:11 -0800

    Part_1: Steering Logic Done
    Steering Logic Done (PASSED Unit Test)

--- projects/lab2_UnitTest_WithMocks/l5_application/Lab2_Part1_Steering/steer_processor.c
+++ projects/lab2_UnitTest_WithMocks/l5_application/Lab2_Part1_Steering/steer_processor.c
@@ -2,11 +2,48 @@
 
 #include "steer_processor.h"
 
+/*================================== steer_processor ==============================
+*@brief:    process steering wheel
+*@para:
+            uint32_t left_sensor_cm ( input )
+            uint32_t right_sensor_cm ( input )
+            pin_num
+*@return:
+            No return
+
+*@Note:
+            Assume THRESH HOLD VALUE:  50 cm
+            objective is to invoke steering function if sensor value is less that threshhold
+            Example: If left sensor is 49 cm, and right is 70 cm --> call steer_right()
+====================================================================================*/
+
 void steer_processor(uint32_t left_sensor_cm, uint32_t right_sensor_cm) {
 
-  if (10 == left_sensor_cm) {
-    steer_left();
-  } else if (10 == right_sensor_cm) {
-    steer_right();
+  // one of them less than thresh hold
+  if ((50 > left_sensor_cm) || (50 > right_sensor_cm)) {
+    // steer_right case:
+    if (50 > left_sensor_cm && 50 < right_sensor_cm) {
+      steer_right();
+    }
+    // steer_left case:
+    else if (50 > right_sensor_cm && 50 < left_sensor_cm) {
+      steer_left();
+    }
   }
-}
+  // Both of them less than thresh hold
+  if ((50 > left_sensor_cm) && (50 > right_sensor_cm)) {
+
+    if (left_sensor_cm < right_sensor_cm) {
+      steer_right();
+    } else if (right_sensor_cm < left_sensor_cm) {
+      steer_left();
+    } else if (50 < left_sensor_cm && left_sensor_cm == right_sensor_cm) {
+      // Do nothing
+    } else if (50 < right_sensor_cm && right_sensor_cm == left_sensor_cm) {
+      // Do nothing
+    }
+  }
+  // Both of them greater than thresh hold
+  if ((50 < left_sensor_cm) && 50 < (right_sensor_cm)) { // Do Nothing
+  }
+};
--- projects/lab2_UnitTest_WithMocks/l5_application/Lab2_Part1_Steering/test/test_steer_processor.c
+++ projects/lab2_UnitTest_WithMocks/l5_application/Lab2_Part1_Steering/test/test_steer_processor.c
@@ -4,27 +4,44 @@
 
 void test_steer_processor__move_left(void) {
   steer_left_Expect();
-  steer_processor(10, 20);
+  steer_processor(51, 49);
 }
 
 void test_steer_processor__move_right(void) {
-  //   steer_right_Expect();
-  //   steer_processor(20, 10);
+  steer_right_Expect();
+  steer_processor(49, 51);
 }
 
-void test_steer_processor__both_sensors_less_than_threshold(void) {}
+void test_steer_processor__both_sensors_less_than_threshold_left_less_right(void) {
+  steer_left_Ignore();
+  steer_right_Expect();
+  steer_processor(40, 45);
+}
 
-// Hint: If you do not setup an Expect()
-// then this test will only pass none of the steer functions is called
-void test_steer_processor__both_sensors_more_than_threshold(void) {}
+void test_steer_processor__both_sensors_less_than_threshold_right_less_left(void) {
+  steer_right_Ignore();
+  steer_left_Expect();
+  steer_processor(45, 40);
+}
 
-// Do not modify this test case
-// Modify your implementation of steer_processor() to make it pass
-// This tests corner case of both sensors below the threshold
+void test_steer_processor__both_sensors_less_than_threshold_butEqual(void) {
+  steer_right_Ignore();
+  steer_left_Ignore();
+  steer_processor(67, 75);
+}
+
+
+void test_steer_processor__both_sensors_more_than_threshold(void) {
+  steer_left_Ignore();
+  steer_right_Ignore();
+  steer_processor(65, 75);
+}
+
+
 void test_steer_processor(void) {
-  // steer_right_Expect();
-  // steer_processor(10, 20);
+  steer_right_Expect();
+  steer_processor(10, 20);
 
-  // steer_left_Expect();
-  // steer_processor(20, 10);
+  steer_left_Expect();
+  steer_processor(20, 10);
 }
